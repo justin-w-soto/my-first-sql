c3c00ff2addb6e5185eebf256810e782c5f35c0c
Justin W Soto <soto@pdx.edu>
2021-08-11 16:36:59 -0700

    squashed that buggit add .!

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -32,43 +32,53 @@ describe('app routes', () => {
 
       const expectation = [
         {
-          id: 1,
+    
           name: 'turkey neck',
           cool_factor: 3,
+          cool_haircut: false
+          
         },
+      
         {
-          id: 2,
+          
           name: 'crab claw',
           cool_factor: 4,
+          cool_haircut: false
         },
+      
         {
-          id: 3,
+         
           name: 'ham fist',
           cool_factor: 4,
+          cool_haircut: false
         },
       
         {
-          id: 4,
+          
           name: 'block head',
           cool_factor: 10,
+          cool_haircut: false
         },
       
         {
-          id: 5,
+          
           name: 'poindexter',
           cool_factor: 1,
+          cool_haircut: false
         },
       
         {
-          id: 6,
+          
           name: 'grandma',
           cool_factor: 10,
+          cool_haircut: true
         },
       
         {
-          id: 7,
+          
           name: 'Gator',
           cool_factor: 8,
+          cool_haircut: false
         }
       ];
 
